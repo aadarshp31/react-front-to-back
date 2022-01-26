331441a69556f1c49452f62e4a53c13a6e958293
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2022-01-26 19:51:34 +0530

    added styles to app

--- feedback-app/src/App.tsx
+++ feedback-app/src/App.tsx
@@ -4,7 +4,7 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header text='Feedback UI' />
+      <Header />
       <div className='container'>
         <h1>My App</h1>
       </div>
--- feedback-app/src/components/Header.tsx
+++ feedback-app/src/components/Header.tsx
@@ -1,12 +1,25 @@
 type Props = {
-  text: string;
+  text?: string;
+  bgColor?: string;
+  textColor?: string;
 };
 
 const Header = (props: Props) => {
+  const {
+    text = 'Feedback UI',
+    bgColor = 'rgba(0, 0, 0, 0.4)',
+    textColor = '#ff6a95',
+  } = props;
+
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+
   return (
-    <header>
+    <header style={headerStyles}>
       <div className='container'>
-        <h2>{props.text}</h2>
+        <h2>{text}</h2>
       </div>
     </header>
   );
