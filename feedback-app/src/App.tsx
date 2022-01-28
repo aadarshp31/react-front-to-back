fc307cb9fac6ce21f58f8b061385e3855ebc912e
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2022-01-29 02:25:47 +0530

    created 'FeedbackStats' component to display stats

--- feedback-app/src/App.tsx
+++ feedback-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
 import Header from './components/Header';
 import FeedbackData from './data/FeedbackData';
 
@@ -17,6 +18,7 @@ function App() {
     <>
       <Header />
       <div className='container'>
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={removeFeedback} />
       </div>
     </>
