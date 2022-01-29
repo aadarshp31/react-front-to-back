2a27f4b152b4740b895c709cb93ff7eae89e3d5d
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2022-01-29 20:55:21 +0530

    added animation to About page using framer-motion

--- feedback-app/src/components/pages/About.tsx
+++ feedback-app/src/components/pages/About.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import Card from '../shared/Card';
 
@@ -5,16 +6,25 @@ type Props = {};
 
 const About = (props: Props) => {
   return (
-    <Card>
-      <div className='about'>
-        <h1>About this Project</h1>
-        <p>This is a React App to leave feedback for a product or service</p>
-        <p>Version: 1.0.0</p>
-        <p>
-          <Link to='/'>Home</Link>
-        </p>
-      </div>
-    </Card>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}>
+        <Card>
+          <div className='about'>
+            <h1>About this Project</h1>
+            <p>
+              This is a React App to leave feedback for a product or service
+            </p>
+            <p>Version: 1.0.0</p>
+            <p>
+              <Link to='/'>Home</Link>
+            </p>
+          </div>
+        </Card>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
