9c3c1090fb56391769de5a3b82f750415825cb61
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2022-02-01 00:13:37 +0530

    added add and remove methods to context

--- feedback-app/src/stateManagement/context/FeedbackContext.tsx
+++ feedback-app/src/stateManagement/context/FeedbackContext.tsx
@@ -1,34 +1,50 @@
 import { createContext, ReactNode, useState } from 'react';
 import IFeedback from '../../entities/IFeedback';
+import IFeedbackContext from '../../entities/IFeedbackContext';
 
-const FeedbackContext = createContext<any>({});
+const initialFeedback: IFeedback[] = [
+  {
+    id: '1',
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: '2',
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: '3',
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+];
+
+const FeedbackContext = createContext<IFeedbackContext>({
+  feedback: initialFeedback,
+  addFeedback: () => console.log('out of context'),
+  removeFeedback: () => console.log('out of context'),
+});
 
 type Props = {
   children: ReactNode;
 };
 
 export const FeedbackProvider = ({ children }: Props) => {
-  const initialFeedback: IFeedback[] = [
-    {
-      id: '1',
-      rating: 10,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-    },
-    {
-      id: '2',
-      rating: 9,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-    },
-    {
-      id: '3',
-      rating: 8,
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-    },
-  ];
-
   const [feedback, setFeedback] = useState(initialFeedback);
+
+  function addFeedback(feedback: IFeedback) {
+    setFeedback((prev) => [feedback, ...prev]);
+  }
+
+  function removeFeedback(id: string) {
+    if (window.confirm('Are you sure you want to delete this rating?')) {
+      setFeedback(feedback.filter((feedbackItem) => feedbackItem.id !== id));
+    }
+  }
+
   return (
-    <FeedbackContext.Provider value={{ feedback, setFeedback }}>
+    <FeedbackContext.Provider value={{ feedback, addFeedback, removeFeedback }}>
       {children}
     </FeedbackContext.Provider>
   );
