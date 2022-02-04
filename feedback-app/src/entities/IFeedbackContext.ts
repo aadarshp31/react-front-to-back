77f4319dbbe64b868b07c5726a431e0063c74566
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2022-02-05 00:10:37 +0530

    updated feedback context interface to contain more states

--- feedback-app/src/entities/IFeedbackContext.ts
+++ feedback-app/src/entities/IFeedbackContext.ts
@@ -2,6 +2,9 @@ import IFeedback from "./IFeedback";
 
 export default interface IFeedbackContext {
   feedback: IFeedback[];
+  feedbackEditObject: { edit: boolean, item: IFeedback | null };
   addFeedback: Function;
   removeFeedback: Function;
+  updateFeedback: Function;
+  editFeedback: Function;
 }
