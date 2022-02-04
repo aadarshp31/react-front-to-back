e6cb38cd270beb66576d087783cbdf64d4d7123c
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2022-02-05 00:43:29 +0530

    added 'edit' icon and functionality to the Feedback item

--- feedback-app/src/components/FeedbackItem.tsx
+++ feedback-app/src/components/FeedbackItem.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaEdit } from 'react-icons/fa';
 import IFeedback from '../entities/IFeedback';
 import FeedbackContext from '../stateManagement/context/FeedbackContext';
 import Card from './shared/Card';
@@ -9,11 +9,14 @@ type Props = {
 };
 
 const FeedbackItem = ({ item }: Props) => {
-  const { removeFeedback } = useContext(FeedbackContext);
+  const { removeFeedback, editFeedback } = useContext(FeedbackContext);
 
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
+      <button className='edit' onClick={() => editFeedback(item)}>
+        <FaEdit size='1.2em' />
+      </button>
       <button className='close' onClick={() => removeFeedback(item.id)}>
         <FaTimes size='1.3em' />
       </button>
