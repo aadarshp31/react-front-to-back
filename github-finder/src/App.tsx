144390fed149bc0c290ebe8427be33ab17a70daa
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2022-02-12 15:01:46 +0530

    added pages 'Home', 'About', and 'NotFound'

--- github-finder/src/App.tsx
+++ github-finder/src/App.tsx
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
+import About from './pages/About';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -9,7 +12,11 @@ function App() {
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
         <main className='container mx-auto px-3 pb-12'>
-          <h1 className='mx-auto'>Github Finder App</h1>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
         </main>
         <Footer />
       </div>
---
+++ github-finder/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const Home = (props: Props) => {
+  return (
+    <div>
+      <h1 className='text-6xl'>Welcome</h1>
+    </div>
+  );
+};
+
+export default Home;
