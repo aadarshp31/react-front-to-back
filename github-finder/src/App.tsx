49ee28ad9c2d064802ab829e13a1a1e1a2219f85
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2022-02-12 14:49:02 +0530

    added footer component

--- github-finder/src/App.tsx
+++ github-finder/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 
 function App() {
@@ -7,9 +8,10 @@ function App() {
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main>
+        <main className='container mx-auto px-3 pb-12'>
           <h1 className='mx-auto'>Github Finder App</h1>
         </main>
+        <Footer />
       </div>
     </Router>
   );
